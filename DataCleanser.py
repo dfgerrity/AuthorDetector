0a4df34f17a44c2eee2c8468e66cf60352d5240d
sccalabr <sccalabr@calpoly.edu>
2014-10-31 00:03:29 -0500

    Added data and DataCleaner scrypt.

---
+++ DataCleanser.py
@@ -0,0 +1,32 @@
+from BeautifulSoup import BeautifulSoup
+
+#This may work. I have not tested, since I cant get BS working
+def removeTags(html):
+    return ''.join(BeautifulSoup(page).findAll(text=True))
+
+def createDataMap():
+    map = {}
+    
+    files = glob.glob("./stackOverFlowData/*.csv")
+    for file in files:
+        with open(file, encoding="latin-1") as csvFile:
+            csvs = csv.reader(csvFile)
+            for row in csvs:
+                question = removeTags(row[0])
+               
+                if question not in map.keys():
+                    userID = removeTags(row[1])
+                    userName = removeTags(row[2])
+                    answer = removeTags(row[3])
+                    
+                    list = []
+                    
+                    list.append(question)
+                    list.append(userID)
+                    list.append(userName)
+                    list.append(answer)
+                    
+                    map[question] = list
+
+    return map
+
